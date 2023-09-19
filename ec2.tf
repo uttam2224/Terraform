e60b15a0e2cd4adfc707dc322112567ce4794cdf
uttam2224 <144069501+uttam2224@users.noreply.github.com>
2023-09-18 20:08:24 -0500

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = var.instance_type
+
+    tags = {
+      Name = "my-first-ec2"
+    }
+}
